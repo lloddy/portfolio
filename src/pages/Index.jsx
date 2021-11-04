ab06182956f7f1abe04f3e6ac4f5c6ce92f7b76c
Lloyd Eddy <lloyd@lloyds-mbp.home>
2021-11-04 15:38:33 -0400

    WIP portfolio images and other styling

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,17 @@
 import logo from '../assets/BandW.svg'
+import Image1 from '../assets/MET.jpeg'
+import Image2 from '../assets/Crit.png'
+import Image3 from '../assets/AvantGarde.png'
 
 const Index = (props) => {
 
 
     return (
         <div className="indexMain">
-            <img src={logo} alt="logo"/>
-            <h1>This is the index</h1>
+            <img src={logo}  alt="logo" id="logo"/>
+            <div>
+                <img src={Image1} alt="scrolling image" id="scrollImage"/>
+            </div>
         </div>
     );
 };
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,23 @@
+import Image1 from '../assets/MET.jpeg'
+import Image2 from '../assets/Crit.png'
+import Image3 from '../assets/AvantGarde.png'
+
 const Portfolio = (props) => {
     return (
         <div>
-            <h1>This is the Portfolio</h1>
+            <h1 className="portfolioTitle">Portfolio</h1>
+            <div className="projects">
+                <div className="project1">
+                    <img src={Image1} alt="MET" id="project1" className="portfolioImages"/>
+                </div>
+                <div className="project2">
+                    <img src={Image2} alt="CritClub" id="project2" className="portfolioImages"/> 
+                </div>
+                <div className="project3">
+                <img src={Image3} alt="Avant-Garde" id="project3" className="portfolioImages"/>
+                </div>
+            </div>
+
         </div>
     );
 };
